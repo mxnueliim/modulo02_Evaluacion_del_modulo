2b94dbe33b0c4e99d6c95c6d372adfdecaec0c20
Manuel I. <flgo.manuelibarram@gmail.com>
2025-06-27 21:30:23 -0400

    ajustes finales

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,28 @@
 $(document).ready(function () {
-
     $('.submitButton').on('click', function (e) {
         e.preventDefault();
 
-        var modalReserva = new bootstrap.Modal(document.getElementById('modalReserva'));
-        modalReserva.show();
+        const formPrincipal = document.querySelector('.needs-validation');
+
+        if (!formPrincipal.checkValidity()) {
+            formPrincipal.classList.add('was-validated');
+        } else {
+            const modalReserva = new bootstrap.Modal(document.getElementById('modalReserva'));
+            modalReserva.show();
+        }
     });
 
     $('.confirmButton').on('click', function (e) {
         e.preventDefault();
-        alert('¡Nunca compartas información confidencial así de facil!');
-    })
+
+        const formModal = document.querySelector('#formConfirmation');
+
+        if (!formModal.checkValidity()) {
+            formModal.classList.add('was-validated');
+        } else {
+            alert('¡Nunca compartas información confidencial así de fácil!');
+            const modal = bootstrap.Modal.getInstance(document.getElementById('modalReserva'));
+            modal.hide();
+        }
+    });
 });
